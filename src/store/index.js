e5468e1003cec22ae5802b3001081311b6df7998
Ruslan <rus5i@>
2024-06-22 23:03:54 +0500

    init

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+// (Vuex Store that contains all modules)
+
+import { createStore } from "vuex";
+import { auth } from "./auth.module";
+
+const store = createStore({
+    modules: {
+        auth,
+    },
+});
+
+export default store;
